c254f8ee9cce746850d4e8549b479cfa499be8f0
arielhandrade88 <arielhandrade@hotmail.com.ar>
2021-12-28 21:20:33 -0300

    creacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { makeStyles } from "@material-ui/core";
+import NavBar from './Components/NavBar/NavBar'
+import About from './Components/About/About'
+import Skills from './Components/Skills/Skills'
+import Works from './Components/Works/Works'
+import Contact from './Components/Contact/Contact'
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <h1>mi portfolio</h1>
+      <NavBar/>
+      <About title="Mi Perfil" id="about" dark={true } />
+      <Skills title="Mis Habilidades" id="skills" dark={false } />
+      <Works title="Mis Proyectos" id="works" dark={true } />
+      <Contact title="Contacto" id="contact" dark={false }/>
     </div>
   );
 }
